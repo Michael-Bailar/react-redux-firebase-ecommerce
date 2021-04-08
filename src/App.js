748cdaa36c900b2df19363bd5c89985f398b027c
Michael-B1764 <mikebailar@gmail.com>
2021-04-08 09:08:16 -0700

    did some styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import './default.scss'
 
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <h2>
+        Hello World!
+      </h2>
     </div>
   );
 }
